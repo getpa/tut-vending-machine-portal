7efc00f77151d06b6ea30e79a264ebda4b371da7
Getpa <catsinf@gmail.com>
2022-04-04 21:49:31 +0900

    fix routing issue

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import './App.css';
  
 function App(){
   return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div className="App">
       <header>
         <h1>TUT 自販機情報ポータル</h1>
-        <a href="/tut-vending-machine-portal">マップ表示</a>
-        <a href="/tut-vending-machine-portal/list">一覧表示</a>
+        <Link to="/">マップ表示</Link>
+        <Link to="/list">一覧表示</Link>
         <p>Safari未対応です…</p>
         <p><a href="https://github.com/getpa/tut-vending-machine-portal">getpa/tut-vending-machine-portal</a></p>
       </header>
 
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Map />} />
           <Route path="/list" element={<ListView />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
  
