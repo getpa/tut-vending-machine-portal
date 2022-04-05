531d495f96dc46160f8c5f11cd16c99efad56f86
Getpa <catsinf@gmail.com>
2022-04-05 12:06:50 +0900

    レイアウト調整

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import './App.css';
 function App(){
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <div className="App">
-      <header>
-        <h1>TUT 自販機情報ポータル</h1>
-        <Link to="/">マップ表示</Link>
-        <Link to="/list">一覧表示</Link>
-        <p>Safari未対応です…</p>
-        <p><a href="https://github.com/getpa/tut-vending-machine-portal">getpa/tut-vending-machine-portal</a></p>
-      </header>
-
-        <Routes>
-          <Route path="/" element={<MapView />} />
-          <Route path="/list" element={<ListView />} />
-        </Routes>
+      <div className="App">
+        <header>
+          <h1>TUT 自販機情報ポータル</h1>
+          <Link to="/">マップ表示</Link>
+          <Link to="/list">一覧表示</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<MapView />} />
+            <Route path="/list" element={<ListView />} />
+          </Routes>
+        </main>
+        <footer>
+          <p>Safari未対応です…</p>
+          <p><a href="https://github.com/getpa/tut-vending-machine-portal">getpa/tut-vending-machine-portal</a></p>
+        </footer>
       </div>
     </BrowserRouter>
   );
